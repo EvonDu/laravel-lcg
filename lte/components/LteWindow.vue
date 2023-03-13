71d82a002892ac5d1e3b990aa6c47e6c58b1ce0c
EvonDu <298550274@qq.com>
2023-03-13 16:11:45 +0800

    完善组件LteWindow的功能

--- lte/components/LteWindow.vue
+++ lte/components/LteWindow.vue
@@ -8,37 +8,32 @@ const props = defineProps({
 
 const data = reactive({
     show: false,
-    model: {},
-    errors: {},
+    data: {},
 });
 
 defineExpose({
-    open : function(model){
-        data.model = JSON.parse(JSON.stringify(model));
+    open: function(data_window, fn_opened){
         data.show = true;
+        data.data = data_window;
+        if(typeof(fn_opened) === "function")
+            fn_opened(data.data);
     },
-    close : function(){
+    close: function(){
         data.show = false;
     },
-    setErrors: function(errors){
-        data.errors = [];
-        for (let key in errors) {
-            if(errors[key] instanceof Array)
-                data.errors[key] = errors[key][0];
-            else
-                data.errors[key] = errors[key];
-        }
-    },
+    data: function(){
+        return data.data;
+    }
 });
 </script>
 
 <template>
     <lte-modal v-model="data.show" :title="title" size="lg">
-        <slot :model="data.model" :errors="data.errors">
+        <slot :data="data.data">
         </slot>
         <template #footer v-if="existSubmit">
             <el-button type="default" @click="data.show = false">取消</el-button>
-            <el-button type="primary" @click="$emit('submit', data.model)">确认</el-button>
+            <el-button type="primary" @click="$emit('submit', data.data)">确认</el-button>
         </template>
     </lte-modal>
 </template>
