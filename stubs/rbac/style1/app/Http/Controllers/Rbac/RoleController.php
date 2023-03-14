094b719b306c41ea3a2f68c39ccff34b577cf875
EvonDu <298550274@qq.com>
2023-03-14 10:34:51 +0800

    完善RBAC功能(单文件版)

--- stubs/rbac/style1/app/Http/Controllers/Rbac/RoleController.php
+++ stubs/rbac/style1/app/Http/Controllers/Rbac/RoleController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Rbac;
 
 use App\Models\Rbac\Role;
 use App\Http\Controllers\Controller;
+use App\Models\Rbac\RoleUser;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Lcg\Http\Responses\ApiResponse;
@@ -53,8 +55,8 @@ class RoleController extends Controller
      * @OA\Post(
      *      path="/api/rbac/roles",
      *      tags={"Role"},
-     *      summary="CREATE",
-     *      description="CREATE",
+     *      summary="Create",
+     *      description="Create",
      *      @OA\RequestBody(required=true, @OA\MediaType(
      *          mediaType="application/json", @OA\Schema(
      *              @OA\Property(description="name", property="name", type="string"),
@@ -78,8 +80,8 @@ class RoleController extends Controller
      * @OA\Get(
      *      path="/api/rbac/roles/{id}",
      *      tags={"Role"},
-     *      summary="GET",
-     *      description="GET",
+     *      summary="Detail",
+     *      description="Detail",
      *      @OA\Parameter(name="id", required=true, in="path", description="id", @OA\Schema(type="integer")),
      *      @OA\Response(response="default", description="result")
      * )
@@ -99,8 +101,8 @@ class RoleController extends Controller
      * @OA\Put(
      *      path="/api/rbac/roles/{id}",
      *      tags={"Role"},
-     *      summary="UPDATE",
-     *      description="UPDATE",
+     *      summary="Update",
+     *      description="Update",
      *      @OA\Parameter(name="id", required=true, in="path", description="id", @OA\Schema(type="integer")),
      *      @OA\RequestBody(required=true, @OA\MediaType(
      *          mediaType="application/json", @OA\Schema(
@@ -134,8 +136,8 @@ class RoleController extends Controller
      * @OA\Delete(
      *      path="/api/rbac/roles/{id}",
      *      tags={"Role"},
-     *      summary="DELETE",
-     *      description="DELETE",
+     *      summary="Delete",
+     *      description="Delete",
      *      @OA\Parameter(name="id", required=true, in="path", description="id", @OA\Schema(type="integer")),
      *      @OA\Response(response="default", description="result")
      * )
@@ -143,6 +145,106 @@ class RoleController extends Controller
     public function destroy(Request $request, $id){
         //Find
         $model = Role::findOne($id);
+        if($model == null)
+            return ApiResponse::NotFound("Not Found");
+
+        //Delete
+        $model->delete();
+
+        //Return
+        return ApiResponse::OK(["message" => "OK"]);
+    }
+
+    /**
+     * API - SEARCH USERS
+     * @OA\Get(
+     *      path="/api/rbac/roles/{id}/search",
+     *      tags={"Role"},
+     *      summary="Search users",
+     *      description="Search users",
+     *      @OA\Parameter(name="id", required=true, in="path", description="role_id", @OA\Schema(type="integer")),
+     *      @OA\Parameter(name="k", required=true, in="query", description="keyword", @OA\Schema(type="integer")),
+     *      @OA\Response(response="default", description="result")
+     * )
+     */
+    public function userSearch(Request $request){
+        $key = $request->input("k", "");
+        $list = User::where('email', 'LIKE', "%{$key}%")->get(["name", "email"]);
+        return ApiResponse::OK([
+            "data" => $list,
+        ]);
+    }
+
+    /**
+     * API - ROLE USER LIST
+     * @OA\Get(
+     *      path="/api/rbac/roles/{id}/users",
+     *      tags={"Role"},
+     *      summary="Role user list",
+     *      description="Role user list",
+     *      @OA\Parameter(name="id", required=true, in="path", description="id", @OA\Schema(type="integer")),
+     *      @OA\Response(response="default", description="result")
+     * )
+     */
+    public function userList(Request $request, $id){
+        //Find
+        $model = Role::findOne($id);
+        if($model == null)
+            return ApiResponse::NotFound("Not Found");
+
+        //Return
+        return ApiResponse::OK(["data" => $model->roleUsers]);
+    }
+
+    /**
+     * API - ROLE USER PUSH
+     * @OA\Post (
+     *      path="/api/rbac/roles/{id}/users",
+     *      tags={"Role"},
+     *      summary="Role user remove",
+     *      description="Role user remove",
+     *      @OA\Parameter(name="id", required=true, in="path", description="role_id", @OA\Schema(type="integer")),
+     *      @OA\RequestBody(required=true, @OA\MediaType(
+     *          mediaType="application/json", @OA\Schema(
+     *              @OA\Property(description="name", property="name", type="string"),
+     *              example={"email":""}
+     *          )
+     *      )),
+     *      @OA\Response(response="default", description="result")
+     * )
+     */
+    public function userPush(Request $request, $id){
+        //Find
+        $email = $request->input("email", "");
+        $user = User::where('email', $email)->first();
+        if($user == null)
+            return ApiResponse::NotFound("Not Found");
+
+        //PUSH
+        $model = new RoleUser();
+        $model->role_id = $id;
+        $model->user_id = $user->id;
+        $model->save();
+
+        //Return
+        return ApiResponse::OK(["message" => "OK"]);
+    }
+
+    /**
+     * API - ROLE USER REMOVE
+     * @OA\Delete(
+     *      path="/api/rbac/roles/{id}/users/{user_id}",
+     *      tags={"Role"},
+     *      summary="Role user remove",
+     *      description="Role user remove",
+     *      @OA\Parameter(name="id", required=true, in="path", description="role_id", @OA\Schema(type="integer")),
+     *      @OA\Parameter(name="user_id", required=true, in="path", description="user_id", @OA\Schema(type="integer")),
+     *      @OA\Response(response="default", description="result")
+     * )
+     */
+    public function userRemove(Request $request, $id, $user_id){
+        //Find
+        $model = RoleUser::findOne(["role_id" => $id, "user_id" => $user_id]);
         if($model == null)
             return ApiResponse::NotFound("Not Found");
 
--- stubs/rbac/style1/resources/js/Pages/Rbac/Role.vue
+++ stubs/rbac/style1/resources/js/Pages/Rbac/Role.vue
@@ -18,7 +18,7 @@ const props = defineProps({
 });
 //数据
 const data = reactive({
-    title: "Role",
+    title: "角色",
     search: {},
     models: [],
     loading: false,
@@ -28,54 +28,57 @@ const data = reactive({
 });
 //方法
 const handleAdd = function(){
-    instance.refs.refAdd.open({});
+    instance.refs.refAdd.open({model: {}});
 };
 const handleEdit = function(model){
-    instance.refs.refEdit.open(model);
+    instance.refs.refEdit.open({model: JSON.parse(JSON.stringify(model))});
 };
 const handleView = function(model){
-    instance.refs.refView.open(model);
+    instance.refs.refView.open({model: JSON.parse(JSON.stringify(model))});
 };
 const handleDelete = function(model){
     ElMessageBox.confirm('此操作将永久删除该数据, 是否继续?', '提示', {confirmButtonText: '确定', cancelButtonText: '取消', type: 'warning'})
         .then(() => {
-            handleDeleteSubmit(model);
+            handleDeleteSubmit({model: JSON.parse(JSON.stringify(model))});
         })
         .catch(() => {
 
         });
 };
 const handleSearch = function(){
-    instance.refs.refSearch.open(data.search);
+    instance.refs.refSearch.open({model: data.search});
 };
 const handleRefresh = function(){
     handleSearchSubmit({});
 };
-const handleAddSubmit = function(model){
+const handleAddSubmit = function(data){
+    let model = data.model;
     axios.post(`${props.api}`, model, {header : {'Content-Type': 'application/json'}}).then(function (response) {
         ElMessage({ type: 'success', grouping: true, message: '添加成功' });
         instance.refs.refAdd.close();
         handleSearchSubmit({});
     }).catch(function(response) {
         if(response.response.data.errors)
-            instance.refs.refAdd.setErrors(response.response.data.errors);
+            instance.refs.refAdd.data().errors = response.response.data.errors;
         if(response.response.data.message)
             ElMessage({ type: 'error', grouping: true, message: response.response.data.message })
     });
 };
-const handleEditSubmit = function(model){
+const handleEditSubmit = function(data){
+    let model = data.model;
     axios.put(`${props.api}/${model.id}`, model, {header : {'Content-Type': 'application/json'}}).then(function (response) {
         ElMessage({ type: 'success', grouping: true, message: '修改成功' });
         instance.refs.refEdit.close();
         handleSearchSubmit({});
     }).catch(function(response) {
         if(response.response.data.errors)
-            instance.refs.refEdit.setErrors(response.response.data.errors);
+            instance.refs.refEdit.data().errors = response.response.data.errors;
         if(response.response.data.message)
             ElMessage({ type: 'error', grouping: true, message: response.response.data.message })
     });
 };
-const handleDeleteSubmit = function(model){
+const handleDeleteSubmit = function(data){
+    let model = data.model;
     axios.delete(`${props.api}/${model.id}`).then(function (response) {
         ElMessage({ type: 'success', grouping: true, message: '删除成功' });
         handleSearchSubmit({});
@@ -85,27 +88,27 @@ const handleDeleteSubmit = function(model){
 };
 const handleSearchSubmit = function(options){
     //params
-    var params = {};
+    let params = {};
     //search
-    var search;
+    let search;
     if(options.keyword){
         search = {name : options.keyword};
     } else {
         search = options.search ? options.search : data.search;
     }
-    for (var key in search){
+    for (let key in search){
         params[key] = search[key];
     }
     //paginate
-    var paginatePage = options?.paginate?.page ? options?.paginate?.page : data.paginate.page;
-    var paginateSize = options?.paginate?.size ? options?.paginate?.size : data.paginate.size;
+    let paginatePage = options?.paginate?.page ? options?.paginate?.page : data.paginate.page;
+    let paginateSize = options?.paginate?.size ? options?.paginate?.size : data.paginate.size;
     if(paginatePage > 1)
         params["page"] = paginatePage;
     if(paginateSize !== 20)
         params["size"] = paginateSize;
     //sort
-    var sortProp = options?.sort?.prop !== undefined ? options?.sort?.prop : data.sort.prop;
-    var sortOrder = options?.sort?.order !== undefined ? options?.sort?.order : data.sort.order;
+    let sortProp = options?.sort?.prop !== undefined ? options?.sort?.prop : data.sort.prop;
+    let sortOrder = options?.sort?.order !== undefined ? options?.sort?.order : data.sort.order;
     if(sortProp)
         params["sort"] = sortProp;
     if(sortOrder)
@@ -129,14 +132,68 @@ const handleSearchSubmit = function(options){
 };
 const handlePermissions = function(model){
     instance.refs.refPermissionsTree.setCheckedKeys(model.permissions,false);
-    instance.refs.refPermissions.open(model);
+    instance.refs.refPermissions.open({model: JSON.parse(JSON.stringify(model))});
 };
-const handlePermissionsSubmit = function(model){
+const handlePermissionsSubmit = function(data){
     let selected = instance.refs.refPermissionsTree.getCheckedKeys(false);
     selected = selected.filter(function (s) { return s && s.trim() });
-    model.permissions  = selected;
-    handleEditSubmit(model);
+    data.model.permissions  = selected;
+    handleEditSubmit(data);
     instance.refs.refPermissions.close();
+};
+const handleAssigns = function(model){
+    let data = {
+        model: JSON.parse(JSON.stringify(model)),
+        search_input: "",
+        search_items: [],
+    }
+    instance.refs.refAssigns.open(data, function(data){
+        handleAssignsLoad()
+    });
+};
+const handleAssignsLoad = function(){
+    let data = instance.refs.refAssigns.data();
+    data.loading = true;
+    axios.get(`${props.api}/${data.model.id}/users`).then(function (response) {
+        data.model.roleUsers = response.data.data;
+    }).finally(function (){
+        data.loading = false;
+    });
+};
+const handleAssignsSearch = function(){
+    let data = instance.refs.refAssigns.data();
+    axios.get(`${props.api}/${data.model.id}/search`, {params: {"k": data.search_input}}).then(function (response) {
+        let items = [];
+        for(let item of response.data.data) {
+            items.push({
+                label : item.email,
+                value : item.email,
+            });
+        }
+        data.search_items = items;
+    });
+};
+const handleAssignsAdd = function(){
+    let data = instance.refs.refAssigns.data();
+    axios.post(`${props.api}/${data.model.id}/users`, {email: data.search_input}).then(function (response) {
+        ElMessage({ type: 'success', grouping: true, message: '添加成功' });
+    }).catch(function(){
+        ElMessage({ type: 'error', grouping: true, message: '添加失败' });
+    }).finally(function (){
+        data.search_input = "";
+        handleAssignsLoad();
+    });
+};
+const handleAssignsRemove = function(item){
+    let data = instance.refs.refAssigns.data();
+    axios.delete(`${props.api}/${data.model.id}/users/${item.id}`).then(function (response) {
+        ElMessage({ type: 'success', grouping: true, message: '移除成功' });
+    }).finally(function (){
+        handleAssignsLoad();
+    });
+};
+const handleAssignsSubmit = function(data){
+    instance.refs.refAssigns.close();
 };
 //计算
 const nodeList = computed(() => {
@@ -175,7 +232,7 @@ handleSearchSubmit({});
                         </el-button-group>
                     </el-col>
                     <el-col :xs="0" :sm="12">
-                        <el-input placeholder="Please input" class="input-with-select" v-model="data.keyword" @keydown.enter="handleSearchSubmit({keyword: data.keyword})">
+                        <el-input placeholder="请输入角色名" class="input-with-select" v-model="data.keyword" @keydown.enter="handleSearchSubmit({keyword: data.keyword})">
                             <template #append>
                                 <el-button type="primary" :icon="Search" @click="handleSearchSubmit({keyword: data.keyword})"/>
                             </template>
@@ -185,58 +242,95 @@ handleSearchSubmit({});
             </div>
             <!-- 表格 -->
             <lte-grid :models="data.models" :loading="data.loading"
-                :paginatePage="data.paginate.page" :paginateSize="data.paginate.size" :paginateTotal="data.paginate.total"
-                @sort-change="handleSearchSubmit({sort: $event})"
-                @page-change="handleSearchSubmit({paginate: $event})">
+                      :paginatePage="data.paginate.page" :paginateSize="data.paginate.size" :paginateTotal="data.paginate.total"
+                      @sort-change="handleSearchSubmit({sort: $event})"
+                      @page-change="handleSearchSubmit({paginate: $event})">
                 <el-table-column prop='id' :label='labels["id"]' sortable='custom' show-overflow-tooltip></el-table-column>
                 <el-table-column prop='name' :label='labels["name"]' sortable='custom' show-overflow-tooltip></el-table-column>
                 <el-table-column prop='created_at' :label='labels["created_at"]' sortable='custom' show-overflow-tooltip></el-table-column>
                 <el-table-column prop='updated_at' :label='labels["updated_at"]' sortable='custom' show-overflow-tooltip></el-table-column>
-                <el-table-column label="操作" width="215px">
+                <el-table-column label="操作" width="260px">
                     <template #default="scope">
                         <el-button-group size="small">
                             <el-button type="info" @click="handleView(scope.row)">详情</el-button>
                             <el-button type="success" @click="handleEdit(scope.row)">编辑</el-button>
-                            <el-button type="primary" @click="handlePermissions(scope.row)">权限</el-button>
+                            <el-button type="primary" @click="handleAssigns(scope.row)">委派</el-button>
+                            <el-button type="warning" @click="handlePermissions(scope.row)">权限</el-button>
                             <el-button type="danger" @click="handleDelete(scope.row)">删除</el-button>
                         </el-button-group>
                     </template>
                 </el-table-column>
             </lte-grid>
             <!-- 查看 -->
-            <lte-window ref="refView" title="详情" v-slot="slot">
-                <el-descriptions size="large" :column="2" border>
-                    <el-descriptions-item :label='labels["id"]'><span v-text='slot.model.id'/></el-descriptions-item>
-                    <el-descriptions-item :label='labels["name"]'><span v-text='slot.model.name'/></el-descriptions-item>
-                    <el-descriptions-item :label='labels["permissions"]'><span v-text='slot.model.permissions'/></el-descriptions-item>
-                    <el-descriptions-item :label='labels["created_at"]'><span v-text='slot.model.created_at'/></el-descriptions-item>
-                    <el-descriptions-item :label='labels["updated_at"]'><span v-text='slot.model.updated_at'/></el-descriptions-item>
+            <lte-window ref="refView" title="详情" v-slot="window">
+                <el-descriptions size="large" :column="2" border v-if="window.data.model">
+                    <el-descriptions-item :label='labels["id"]'><span v-text='window.data.model.id'/></el-descriptions-item>
+                    <el-descriptions-item :label='labels["name"]'><span v-text='window.data.model.name'/></el-descriptions-item>
+                    <el-descriptions-item :label='labels["permissions"]'><span v-text='window.data.model.permissions'/></el-descriptions-item>
+                    <el-descriptions-item :label='labels["created_at"]'><span v-text='window.data.model.created_at'/></el-descriptions-item>
+                    <el-descriptions-item :label='labels["updated_at"]'><span v-text='window.data.model.updated_at'/></el-descriptions-item>
                 </el-descriptions>
             </lte-window>
             <!-- 添加 -->
-            <lte-window ref="refAdd" title="添加" v-slot="slot" :existSubmit="true" @submit="handleAddSubmit">
-                <el-form label-width="100px">
-                    <el-form-item :label='labels["name"]' :error='slot.errors.name'><el-input v-model='slot.model.name'></el-input></el-form-item>
+            <lte-window ref="refAdd" title="添加" v-slot="window" :existSubmit="true" @submit="handleAddSubmit">
+                <el-form label-width="100px" v-if="window.data.model">
+                    <el-form-item :label='labels["name"]' :error='window?.data?.errors?.name?.[0]'><el-input v-model='window.data.model.name'></el-input></el-form-item>
                 </el-form>
             </lte-window>
             <!-- 编辑 -->
-            <lte-window ref="refEdit" title="编辑" v-slot="slot" :existSubmit="true" @submit="handleEditSubmit">
-                <el-form label-width="100px">
-                    <el-form-item :label='labels["name"]' :error='slot.errors.name'><el-input v-model='slot.model.name'></el-input></el-form-item>
+            <lte-window ref="refEdit" title="编辑" v-slot="window" :existSubmit="true" @submit="handleEditSubmit">
+                <el-form label-width="100px" v-if="window.data.model">
+                    <el-form-item :label='labels["name"]' :error='window?.data?.errors?.name?.[0]'><el-input v-model='window.data.model.name'></el-input></el-form-item>
                 </el-form>
             </lte-window>
             <!-- 搜索 -->
-            <lte-window ref="refSearch" title="搜索" v-slot="slot" :existSubmit="true" @submit="handleSearchSubmit({search:$event})">
-                <el-form label-width="100px">
-                    <el-form-item :label='labels["id"]'><el-input v-model='slot.model.id'></el-input></el-form-item>
-                    <el-form-item :label='labels["name"]'><el-input v-model='slot.model.name'></el-input></el-form-item>
-                    <el-form-item :label='labels["permissions"]'><el-input v-model='slot.model.permissions'></el-input></el-form-item>
+            <lte-window ref="refSearch" title="搜索" v-slot="window" :existSubmit="true" @submit="handleSearchSubmit({search:$event})">
+                <el-form label-width="100px" v-if="window.data.model">
+                    <el-form-item :label='labels["id"]'><el-input v-model='window.data.model.id'></el-input></el-form-item>
+                    <el-form-item :label='labels["name"]'><el-input v-model='window.data.model.name'></el-input></el-form-item>
+                    <el-form-item :label='labels["permissions"]'><el-input v-model='window.data.model.permissions'></el-input></el-form-item>
                 </el-form>
             </lte-window>
             <!-- 权限 -->
-            <lte-window ref="refPermissions" title="权限" v-slot="slot" :existSubmit="true" @submit="handlePermissionsSubmit">
+            <lte-window ref="refPermissions" title="权限" v-slot="window" :existSubmit="true" @submit="handlePermissionsSubmit">
                 <el-tree ref="refPermissionsTree" node-key="code" :data="nodeList" show-checkbox default-expand-all/>
+            </lte-window>
+            <!-- 委派 -->
+            <lte-window ref="refAssigns" title="委派" v-slot="window" :existSubmit="true" @submit="handleAssignsSubmit">
+                <el-form label-width="100px" v-if="window.data.model">
+                    <div class="option">
+                        <el-select-v2
+                            v-model="window.data.search_input"
+                            filterable
+                            remote
+                            :remote-method="handleAssignsSearch"
+                            :options="window.data.search_items"
+                            placeholder="请输入邮箱搜索用户"
+                        />
+                        <el-button type="success" :icon="Plus" @click="handleAssignsAdd()">添加</el-button>
+                    </div>
+                    <el-table :data="window.data.model.roleUsers" v-if="window.data.model.roleUsers" style="width: 100%">
+                        <el-table-column label="用户" prop="name"/>
+                        <el-table-column label="邮箱" prop="email"/>
+                        <el-table-column label="操作">
+                            <template #default="scope">
+                                <el-button-group size="small">
+                                    <el-button type="danger" @click="handleAssignsRemove(scope.row)">移除</el-button>
+                                </el-button-group>
+                            </template>
+                        </el-table-column>
+                    </el-table>
+                </el-form>
             </lte-window>
         </lte-card>
     </lte-layout>
 </template>
+
+<style scoped>
+.option{
+    margin-bottom: 12px;
+}
+.option *{
+    margin-right: 6px;
+}
+</style>
