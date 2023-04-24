5775d812f387933c15cde95a81f1f8a3cf5923fd
EvonDu <298550274@qq.com>
2023-04-24 14:25:51 +0800

    vite添加默认根路径配置(base)

--- stubs/inertia-vite/vite.config.js
+++ stubs/inertia-vite/vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    base: './',
     plugins: [
         laravel({
             input: 'resources/js/app.js',
